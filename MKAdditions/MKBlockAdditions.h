5fc868e464505dc3806393c42dc747aba0adbafe
Ken Cooper <ken@coopercode.net>
2015-01-29 15:01:38 -0800

    Conversion to NSInteger - 64 bit warning fix.

--- MKAdditions/MKBlockAdditions.h
+++ MKAdditions/MKBlockAdditions.h
@@ -8,7 +8,7 @@
 
 typedef void (^VoidBlock)();
 
-typedef void (^DismissBlock)(int buttonIndex);
+typedef void (^DismissBlock)(NSInteger buttonIndex);
 typedef void (^CancelBlock)();
 typedef void (^PhotoPickedBlock)(UIImage *chosenImage);
 
